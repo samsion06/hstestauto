0214e77c02d96257632bade4bdfd984c2b63800a
zhouxiongxin <424468708@qq.com>
2020-01-02 18:04:38 +0800

    update

--- src/main/java/com/example/utils/DataTransferUtil.java
+++ src/main/java/com/example/utils/DataTransferUtil.java
@@ -140,12 +140,6 @@ public class DataTransferUtil {
         Reporter.log("修改用户身份状态_"+incomeMessage+builder+ "}");
         return bytes;
     }
-
-
-
-
-
-
     //获取用户基础信息
     public static ByteArrayEntity UserInfoRequest(Integer channelId,String channelUserId){
         UserBaseServiceProto.UserInfoRequest.Builder builder=UserBaseServiceProto.UserInfoRequest.newBuilder();
@@ -524,5 +518,12 @@ public class DataTransferUtil {
     }
 
 
+    /**
+     * 团长关系信息
+     */
+    public static ByteArrayEntity UserTeamRelationRegisterRequest(){
 
+
+        return  null;
+    }
 }
