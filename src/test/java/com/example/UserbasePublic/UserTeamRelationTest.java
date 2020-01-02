80f4b2bf5b3ecafd7358ac735c8535f97be5adac
zhouxiongxin <424468708@qq.com>
2020-01-02 17:59:49 +0800

    update

---
+++ src/test/java/com/example/UserbasePublic/UserTeamRelationTest.java
@@ -0,0 +1,13 @@
+package com.example.UserbasePublic;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest
+public class UserTeamRelationTest extends AbstractTestNGSpringContextTests {
+
+
+
+
+
+}
