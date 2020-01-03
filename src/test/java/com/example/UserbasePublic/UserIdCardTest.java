4a56eb4b29f4d5ac31d69f4039624a53719932f7
zhouxiongxin <424468708@qq.com>
2020-01-03 11:32:00 +0800

    update

--- src/test/java/com/example/UserbasePublic/UserIdCardTest.java
+++ src/test/java/com/example/UserbasePublic/UserIdCardTest.java
@@ -34,15 +34,16 @@ public class UserIdCardTest extends AbstractTestNGSpringContextTests {
 
     @org.testng.annotations.Test(description = "1.实名认证" +
                                                "2.实名认证查询")
-    public void queryStatus() {
+    public void identifyAndQueryStatus() {
 
         String attachmentUrl="http://images.huasheng100.com/public/1573632666839292.jpg;http://images.huasheng100.com/public/1573632674240333.jpg";
         String channeluserId = "56903884418";
         String realName="尹小芳";
         String idCardNum="430525199204064942";
 
-        //实名认证
+            //实名认证 主要看userbase的字段
         try {
+
             httpClient = HttpClients.createDefault();
             uri = new URI(HttpConfigUtil.scheme, HttpConfigUtil.url, "/user/idCard/identify", "");
             post = new HttpPost(uri);
@@ -60,6 +61,7 @@ public class UserIdCardTest extends AbstractTestNGSpringContextTests {
             post.setHeader("Content-Type", "application/x-protobuf");
             response = httpClient.execute(post);
             CheckReponseResult.AssertResponses(response, UserIdCardIdentifyServiceProto.UserIdCardIdentifyInfo.class);
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
