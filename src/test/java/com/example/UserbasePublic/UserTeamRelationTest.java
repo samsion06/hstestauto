fa3c0b879fe5b6a789b92eaa7e289c40739ad131
zhouxiongxin <424468708@qq.com>
2020-01-06 15:03:55 +0800

    update

--- src/test/java/com/example/UserbasePublic/UserTeamRelationTest.java
+++ src/test/java/com/example/UserbasePublic/UserTeamRelationTest.java
@@ -15,7 +15,6 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.net.URI;
 
