e46af578e0a865e13163ba3bc637e59efa4ab4e9
zhouxiongxin <424468708@qq.com>
2020-01-10 09:20:08 +0800

    update

--- src/test/java/com/example/UserbasePublic/UserTeamRelationTest.java
+++ src/test/java/com/example/UserbasePublic/UserTeamRelationTest.java
@@ -85,7 +85,7 @@ public class UserTeamRelationTest extends AbstractTestNGSpringContextTests {
     }
 
 
-   // @Test(description = "3.团长关系聚合")
+    @Test(description = "3.团长关系聚合")
     public void teamRelationCURD(){
         UserRleationInfo userRleationInfo =new UserRleationInfo();
         userRleationInfo.setAppType(1);
