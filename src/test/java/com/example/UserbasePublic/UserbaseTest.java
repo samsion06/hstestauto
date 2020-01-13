8d36b5d0aa0471f6a1feaa6c152234ab1ecb95dd
zhouxiongxin <424468708@qq.com>
2020-01-13 15:23:34 +0800

    update

--- src/test/java/com/example/UserbasePublic/UserbaseTest.java
+++ src/test/java/com/example/UserbasePublic/UserbaseTest.java
@@ -69,6 +69,7 @@ public class UserbaseTest extends AbstractTestNGSpringContextTests {
         HsrjUserInfo hsrjUserInfo=new HsrjUserInfo();
         hsrjUserInfo.setPushNo(pushNo);
         hsrjUserInfo.setChannelUserId("176735");
+        hsrjUserInfo.setUserTagStatus(2);
 
         try {
             //登录
@@ -127,9 +128,19 @@ public class UserbaseTest extends AbstractTestNGSpringContextTests {
             Assert.assertEquals("{\"msg\": \"RESP_CODE_SUCCESS\"}", jsonFormat.printToString(respc));
             DataUtils.logResponse(jsonFormat.printToString(respc));
             //检查数据库
+            CheckDatabase.CheckDatabaseHsrjUserInfo(userBaseInfoMapper,"pushNoUpdate",hsrjUserInfo);
 
-
-
+            //修改用户标签
+            uri = new URI(HttpConfigUtil.scheme, null, "user-base.huasheng100.com", 80, "/base/user/tag/update", "", null);
+            post = new HttpPost(uri);
+            byteArrayEntity = DataTransferUtil.UserInfoTagRequest("178803",3);
+            post.setEntity(byteArrayEntity);
+            post.setHeader("Content-Type", "application/x-protobuf");
+            response = httpClient.execute(post);
+            String userStatusTag = CheckReponseResult.AssertResponse(response);
+            Assert.assertEquals("RESP_CODE_SUCCESS", userStatusTag);
+            //检查数据库
+            CheckDatabase.CheckDatabaseHsrjUserInfo(userBaseInfoMapper,"userTagStatusUpdate",hsrjUserInfo);
         } catch (Exception e) {
             e.printStackTrace();
         }
