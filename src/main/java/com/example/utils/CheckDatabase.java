420d0cf51a47305108e3413fbcc74554a4dd7d27
zhouxiongxin <424468708@qq.com>
2020-01-13 14:49:14 +0800

    update

--- src/main/java/com/example/utils/CheckDatabase.java
+++ src/main/java/com/example/utils/CheckDatabase.java
@@ -7,28 +7,31 @@ import org.testng.Reporter;
 
 public class CheckDatabase {
 
-    private static UserAliPayInfo dataUserAliPayInfo;
-    private static UserWeChatInfo dataUserWeChatInfos;
-    private static UserAddressInfo dataUserAddressInfo;
+    private static UserAliPayInfo dataBaseUserAliPayInfo;
+    private static UserWeChatInfo dataBaseUserWeChatInfos;
+    private static UserAddressInfo dataBaseUserAddressInfo;
     private static UserBaseInfo dataBaseUserBaseInfo;
-    private static UserLoginInfo dataUserLoginInfo;
-    private static UserTaobaoInfo dataUserTaobaoInfo;
+    private static UserLoginInfo dataBaseUserLoginInfo;
+    private static UserTaobaoInfo dataBaseUserTaobaoInfo;
     private static UserTeamInfo dataBaseUserTeamInfo;
     private static UserRleationInfo dataBaseUserRleationInfo;
+    private static HsrjUserInfo dataBaseHsrjUserInfo;
+
+
 
     //数据库检查
     public static void CheckDatabaseUserUserWeChatInfo(UserBaseInfoMapper userBaseInfoMapper,String method, UserWeChatInfo userWeChatInfo) {
         switch (method) {
             //微信解绑
             case "WeChatInfoUnbind"://微信解除绑定
-                dataUserWeChatInfos = userBaseInfoMapper.queryWeChatInfo(userWeChatInfo.getChannelUserId());
-                Assert.assertEquals(userWeChatInfo.getIsDelete(), dataUserWeChatInfos.getIsDelete());
-                DataUtils.logDatabase(1,"is_delete",dataUserWeChatInfos.getIsDelete().toString());
+                dataBaseUserWeChatInfos = userBaseInfoMapper.queryWeChatInfo(userWeChatInfo.getChannelUserId());
+                Assert.assertEquals(userWeChatInfo.getIsDelete(), dataBaseUserWeChatInfos.getIsDelete());
+                DataUtils.logDatabase(1,"is_delete",dataBaseUserWeChatInfos.getIsDelete().toString());
                 break;
             case "WeChatInfoBind": //微信绑定
-                dataUserWeChatInfos = userBaseInfoMapper.queryWeChatInfo(userWeChatInfo.getChannelUserId());
-                Assert.assertEquals(userWeChatInfo.getChannelUserId(), dataUserWeChatInfos.getChannelUserId());
-                DataUtils.logDatabase(2, null, dataUserWeChatInfos.toString());
+                dataBaseUserWeChatInfos = userBaseInfoMapper.queryWeChatInfo(userWeChatInfo.getChannelUserId());
+                Assert.assertEquals(userWeChatInfo.getChannelUserId(), dataBaseUserWeChatInfos.getChannelUserId());
+                DataUtils.logDatabase(2, null, dataBaseUserWeChatInfos.toString());
                 break;
         }
     }
@@ -36,15 +39,15 @@ public class CheckDatabase {
     public static void CheckDatabaseUserUserAddressInfo(UserBaseInfoMapper userBaseInfoMapper,String method,UserAddressInfo userAddressInfo){
         switch (method) {
             case "AddressUpadate"://更新收货地址
-                dataUserAddressInfo = userBaseInfoMapper.queryUserAddressInfo(userAddressInfo.getChannelUserId());
+                dataBaseUserAddressInfo = userBaseInfoMapper.queryUserAddressInfo(userAddressInfo.getChannelUserId());
                 String name = userAddressInfo.getName();
-                Assert.assertEquals(userAddressInfo.getName(), dataUserAddressInfo.getName()); //名称是否有更新
-                DataUtils.logDatabase(1,"name",dataUserAddressInfo.getName());
+                Assert.assertEquals(userAddressInfo.getName(), dataBaseUserAddressInfo.getName()); //名称是否有更新
+                DataUtils.logDatabase(1,"name",dataBaseUserAddressInfo.getName());
                 break;
             case "AddressDelete"://删除收货地址
-                dataUserAddressInfo = userBaseInfoMapper.queryUserAddressInfo(userAddressInfo.getChannelUserId());
-                Assert.assertEquals(userAddressInfo.getIsDelete(), dataUserAddressInfo.getIsDelete());
-                DataUtils.logDatabase(1,"is_delete", dataUserAddressInfo.getIsDelete().toString());
+                dataBaseUserAddressInfo = userBaseInfoMapper.queryUserAddressInfo(userAddressInfo.getChannelUserId());
+                Assert.assertEquals(userAddressInfo.getIsDelete(), dataBaseUserAddressInfo.getIsDelete());
+                DataUtils.logDatabase(1,"is_delete", dataBaseUserAddressInfo.getIsDelete().toString());
                 break;
         }
     }
@@ -53,20 +56,20 @@ public class CheckDatabase {
     public static void CheckDatabaseUserAliPayInfo(UserBaseInfoMapper userBaseInfoMapper,String method,UserAliPayInfo userAliPayInfo){
         switch (method) {
             case "AliPayBind"://支付宝绑定
-                dataUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
-                Assert.assertEquals(userAliPayInfo.getChannelUserId(),dataUserAliPayInfo.getChannelUserId());
-                DataUtils.logDatabase(2, null, dataUserAliPayInfo.toString());
+                dataBaseUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
+                Assert.assertEquals(userAliPayInfo.getChannelUserId(),dataBaseUserAliPayInfo.getChannelUserId());
+                DataUtils.logDatabase(2, null, dataBaseUserAliPayInfo.toString());
                 break;
             case "AliPayAuth"://支付宝授权
-                dataUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
-                Assert.assertEquals(userAliPayInfo.getStatus(),dataUserAliPayInfo.getStatus());
-                DataUtils.logDatabase(1,"Status",dataUserAliPayInfo.getStatus()+"");
+                dataBaseUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
+                Assert.assertEquals(userAliPayInfo.getStatus(),dataBaseUserAliPayInfo.getStatus());
+                DataUtils.logDatabase(1,"Status",dataBaseUserAliPayInfo.getStatus()+"");
                 break;
             case "AliPayCancel"://取消授权
-                dataUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
+                dataBaseUserAliPayInfo = userBaseInfoMapper.queryAliPayInfo(userAliPayInfo.getChannelUserId());
                 int unbindStatus = userAliPayInfo.getStatus();
-                Assert.assertEquals(userAliPayInfo.getStatus(),dataUserAliPayInfo.getStatus());
-                DataUtils.logDatabase(1,"Status",dataUserAliPayInfo.getStatus().toString());
+                Assert.assertEquals(userAliPayInfo.getStatus(),dataBaseUserAliPayInfo.getStatus());
+                DataUtils.logDatabase(1,"Status",dataBaseUserAliPayInfo.getStatus().toString());
                 break;
         }
     }
@@ -75,14 +78,14 @@ public class CheckDatabase {
     public static void CheckDatabaseUserTaobaoInfo(UserBaseInfoMapper userBaseInfoMapper,String method,UserTaobaoInfo userTaobaoInfo){
         switch (method) {
             case "TaoBaoAuth"://淘宝授权
-                dataUserTaobaoInfo = userBaseInfoMapper.queryUserTaobaoInfo(userTaobaoInfo.getChannelUserId());
-                Assert.assertEquals(userTaobaoInfo.getChannelUserId(),dataUserTaobaoInfo.getChannelUserId());
-                DataUtils.logDatabase(2, null, dataUserTaobaoInfo.toString());
+                dataBaseUserTaobaoInfo = userBaseInfoMapper.queryUserTaobaoInfo(userTaobaoInfo.getChannelUserId());
+                Assert.assertEquals(userTaobaoInfo.getChannelUserId(),dataBaseUserTaobaoInfo.getChannelUserId());
+                DataUtils.logDatabase(2, null, dataBaseUserTaobaoInfo.toString());
                 break;
             case "TaoBaoCancel"://取消授权
-                dataUserTaobaoInfo = userBaseInfoMapper.queryUserTaobaoInfo(userTaobaoInfo.getChannelUserId());
+                dataBaseUserTaobaoInfo = userBaseInfoMapper.queryUserTaobaoInfo(userTaobaoInfo.getChannelUserId());
                 Assert.assertEquals(userTaobaoInfo.getCreateTime(),null);
-                DataUtils.logDatabase(2, null, dataUserTaobaoInfo.toString());
+                DataUtils.logDatabase(2, null, dataBaseUserTaobaoInfo.toString());
                 break;
         }
     }
@@ -103,35 +106,37 @@ public class CheckDatabase {
                 DataUtils.logDatabase(1,"head_img",dataBaseUserBaseInfo.getHeadImg());
                 break;
             case "MobileUpdate"://修改手机
-                dataUserLoginInfo = userBaseInfoMapper.queryUserLoginInfo(userLoginInfo.getChannelUserId());
-                System.out.println(dataUserLoginInfo.getLoginName());
-                Assert.assertEquals(userLoginInfo.getLoginName(), dataUserLoginInfo.getLoginName());
-                DataUtils.logDatabase(1,"login_name",dataUserLoginInfo.getLoginName());
+                dataBaseUserLoginInfo = userBaseInfoMapper.queryUserLoginInfo(userLoginInfo.getChannelUserId());
+                System.out.println(dataBaseUserLoginInfo.getLoginName());
+                Assert.assertEquals(userLoginInfo.getLoginName(), dataBaseUserLoginInfo.getLoginName());
+                DataUtils.logDatabase(1,"login_name",dataBaseUserLoginInfo.getLoginName());
                 break;
             case "PwdUpdate"://修改密码
-                dataUserLoginInfo = userBaseInfoMapper.queryUserLoginInfo(userLoginInfo.getChannelUserId());
-                Assert.assertEquals(userLoginInfo.getLoginPwd(), dataUserLoginInfo.getLoginPwd());
-                DataUtils.logDatabase(1,"login_pwd",dataUserLoginInfo.getLoginPwd());
+                dataBaseUserLoginInfo = userBaseInfoMapper.queryUserLoginInfo(userLoginInfo.getChannelUserId());
+                Assert.assertEquals(userLoginInfo.getLoginPwd(), dataBaseUserLoginInfo.getLoginPwd());
+                DataUtils.logDatabase(1,"login_pwd",dataBaseUserLoginInfo.getLoginPwd());
                 break;
             case "StatusUpdate"://修改用户身份状态
                 dataBaseUserBaseInfo = userBaseInfoMapper.queryUserBaseInfo(userLoginInfo.getChannelUserId());
                 Assert.assertEquals(userBaseInfo.getStatus(), dataBaseUserBaseInfo.getStatus());
                 DataUtils.logDatabase(1,"status",dataBaseUserBaseInfo.getStatus().toString());
                 break;
+        }
+    }
+    public static void CheckDatabaseHsrjUserInfo(UserBaseInfoMapper userBaseInfoMapper,String method,HsrjUserInfo hsrjUserInfo){
+        switch (method) {
             case "pushNoUpdate"://更新邀请码
-
-
-
-
-
-
-
-
+                dataBaseHsrjUserInfo=userBaseInfoMapper.queryHsrjUserInfo(hsrjUserInfo.getChannelUserId());
+                Assert.assertEquals(hsrjUserInfo.getPushNo(), dataBaseHsrjUserInfo.getPushNo());
+                DataUtils.logDatabase(1,"pushNo",dataBaseHsrjUserInfo.getPushNo().toString());
+            case "userTagStatusUpdate": //更新用户标签
 
 
 
         }
     }
+
+
 
     //团长信息
     public static void CheckDatabaseUserTeamInfo(TeamRealtionInfoMapper teamRealtionInfoMapper, String method, UserTeamInfo userTeamInfo) {
